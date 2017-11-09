5f8c30d202a5d0ad0ca95d54a5a4fc1263b6875c
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2017-11-08 22:00:32 -0800

    Added todo items.

--- add_weeks.py
+++ add_weeks.py
@@ -1,6 +1,12 @@
 import sys
 from datetime import datetime, timedelta
 
+# Script to add a given number of weeks to a date.
+
+# Todo:- (1) use argparse instead of parsing sys.argv directly.
+# (2) Extend the script to any number of arguments. For example,
+# script_name <dt> offset1 offset2 offset3 ...
+
 dt = sys.argv[1]
 offset = int(sys.argv[2])
 if len(dt) == 8:
