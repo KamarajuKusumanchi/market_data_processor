d855c07e2d3054ad8fba0ddeee7964cd2ca02ec5
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2017-11-25 21:32:28 -0800

    Automatically check all python files for pep8 conformance.

---
+++ list_files.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+
+import os
+
+
+def list_files_by_extension(path, extension):
+    # sample usage:
+    # list_files_by_extension('.', '.py')
+    # list_files_by_extension('.', ('.py'))
+    # list_files_by_extension('.', ('.html', '.htm'))
+    file_list = [os.path.join(root, file)
+                 for root, dirs, files in os.walk(path)
+                 for file in files
+                 if file.endswith(extension)]
+    return file_list
+
+
+def list_python_files(path):
+    return list_files_by_extension(path, '.py')
+
+if __name__ == "__main__":
+    # files = list_files_by_extension('.', '.py')
+    # files = list_files_by_extension('.', ('.py'))
+    # files = list_files_by_extension('.', ('.py', '.txt'))
+    files = list_python_files('.')
+    print(*files, sep='\n')
--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -18,10 +18,13 @@ class Pep8ConformanceTestCase(unittest.TestCase):
     '''Test that all code conforms to pep8 standard'''
 
     def test_pep8_conformance(self):
-        self.pep8style = pep8.StyleGuide(show_source=True)
-        files = (['add_weeks.py', 'google_finance.py'])
-        self.pep8style.check_files(files)
-        self.assertEqual(self.pep8style.options.report.total_errors, 0)
+        pep8style = pep8.StyleGuide(show_source=True)
+        # files = (['add_weeks.py', 'google_finance.py'])
+        from list_files import list_python_files
+        files = list_python_files('.')
+        print('Checking', files, 'for pep8 conformance.')
+        pep8style.check_files(files)
+        self.assertEqual(pep8style.options.report.total_errors, 0)
 
 
 if __name__ == '__main__':
