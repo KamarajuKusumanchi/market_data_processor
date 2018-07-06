2975a7d7d554b50541926f915df27c7c92942c8d
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-07-06 15:42:11 -0400

    fix test cases

--- key_stats.py
+++ key_stats.py
@@ -15,7 +15,9 @@ import pandas as pd
 
 def get_html(url):
     # Get html from url
-    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) ' \
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+        'Chrome/66.0.3359.139 Safari/537.36'
     header = {'User-Agent': user_agent}
     response = requests.get(url, headers=header)
     return response.text
