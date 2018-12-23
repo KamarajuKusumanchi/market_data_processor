4e2b847da8befb67efe15f2f4006ff9942dffcd1
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-12-22 23:53:27 -0500

    Add stuff

--- sp500_tickers.py
+++ sp500_tickers.py
@@ -8,12 +8,18 @@ from datetime import datetime
 import sys
 
 
-def get_sp500_table():
-    '''
+def get_sp500_table(filter_columns=None):
+    """
     Parse a wikipedia article to extract information on the S&P 500 companies
-    :return: Data Frame containing company tickers, names, sector
-    information etc., of all the companies in S&P 500.
-    '''
+    :param filter_columns: columns to filter. It can either be a list or
+    a string or None.
+    * List - list of columns to filter
+    * string - filter that column
+    * None - return all columns
+    :return: By default, returns a Data Frame containing company tickers,
+    names, sector, information etc., of all the S&P 500 companies. If
+    filter_columns is supplied it returns only those columns.
+    """
     # Todo:- Add a test case for this function.
     url = "https://en.wikipedia.org/wiki/List_of_S&P_500_companies"
     resp = requests.get(url)
@@ -30,23 +36,27 @@ def get_sp500_table():
                         for cell in row.findAll('td')]
                        for row in rows[1:]],
                       columns=columns)
+
+    if filter_columns is not None:
+        if isinstance(filter_columns, str):
+            filter_columns = [filter_columns]
+        return df[filter_columns]
     return df
 
 
 def get_sp500_tickers():
     '''Get the list of S&P 500 tickers'''
-    coi = 'Symbol'
-    return get_sp500_table()[coi].tolist()
+    coi = ['Symbol']
+    return get_sp500_table(coi).tolist()
 
 
 def get_sp500_df():
-    table = get_sp500_table()
+    coi = 'Symbol'
+    table = get_sp500_table(coi)
     today = datetime.today().strftime('%Y%m%d')
     # This will give a dataframe with YYYYMMDD as the row index, tickers as
     # columns.
-    coi = 'Symbol'
-    tickers = table[[coi]]\
-        .sort_values(by=coi)\
+    tickers = table.sort_values(by=coi)\
         .transpose()\
         .rename({coi: today})
     return tickers
