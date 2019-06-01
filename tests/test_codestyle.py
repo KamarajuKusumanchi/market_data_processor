b278d2772e8ce07973c6bb6247e5ed36b018728b
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2019-06-01 15:18:22 -0400

    change pep8 to pycodestyle

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -1,8 +1,9 @@
 import unittest
-try:
-    from pycodestyle import StyleGuide
-except ImportError:
-    from pep8 import StyleGuide
+# try:
+#     from pycodestyle import StyleGuide
+# except ImportError:
+#     from pep8 import StyleGuide
+from pycodestyle import StyleGuide
 
 
 # To run the tests
@@ -27,17 +28,17 @@ except ImportError:
 class Pep8ConformanceTestCase(unittest.TestCase):
     '''Test that all code conforms to pep8 standard'''
 
-    def test_pep8_conformance(self):
+    def test_codestyle_conformance(self):
         # Ignore the following errors:
         # E501 line too long (xxx > 79 characters)
-        pep8style = StyleGuide(show_source=True, ignore=['E501'])
+        codestyle = StyleGuide(show_source=True, ignore=['E501'])
         # files = (['add_weeks.py', 'google_finance.py'])
         from list_files import list_python_files
         files = list_python_files('.')
-        print('Checking', files, 'for pep8 conformance.')
-        report = pep8style.check_files(files)
+        print('Checking', files, 'for codestyle conformance.')
+        report = codestyle.check_files(files)
         report.print_statistics()
-        self.assertEqual(pep8style.options.report.total_errors, 0)
+        self.assertEqual(codestyle.options.report.total_errors, 0)
 
 
 if __name__ == '__main__':
