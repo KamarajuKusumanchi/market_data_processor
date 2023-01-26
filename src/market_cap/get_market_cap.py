2674e26409c20fcead274dd7a9d7c5426acc408d
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-01-26 10:58:30 -0500

    lgtm

--- src/market_cap/get_market_cap.py
+++ src/market_cap/get_market_cap.py
@@ -1,10 +1,19 @@
 #! /usr/bin/env python3
 # Get market cap
+import argparse
 
 import requests
 import pandas as pd
 import pprint
 import os
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Get market cap")
+    parser.add_argument(
+        "--limit", action="store", dest="limit", help="Limit to N entries", default=None
+    )
+    return parser
 
 
 def get_nasdaq_data():
@@ -59,23 +68,32 @@ def convert_nasdaq_data_to_df(data):
     return df
 
 
-def get_market_cap(nasdaq_df):
+def get_market_cap(nasdaq_df, limit):
     """
     :param nasdaq_df: dataFrame
     :return: dataFrame
     """
     market_cap = nasdaq_df[["symbol", "marketCap"]]
-    market_cap = market_cap.sort_values("marketCap", ascending=False)
+    market_cap = market_cap.sort_values("marketCap", ascending=False, ignore_index=True)
+    if limit:
+        market_cap = market_cap.loc[: limit - 1, :]
+    market_cap["pct_weight"] = (
+        market_cap["marketCap"] / market_cap["marketCap"].sum()
+    ) * 100
     return market_cap
 
 
 def run_code():
+    parser = create_parser()
+    args = parser.parse_args()
+    limit = int(args.limit) if args.limit else None
     nasdaq_data = get_nasdaq_data()
     # dump_nasdaq_data(nasdaq_data)
     nasdaq_df = convert_nasdaq_data_to_df(nasdaq_data)
-    market_cap = get_market_cap(nasdaq_df)
+    market_cap = get_market_cap(nasdaq_df, limit)
     pd.set_option("display.max_columns", None, "display.max_rows", None)
-    print(market_cap.to_csv(index=False))
+    # print(market_cap.to_csv(index=False))
+    print(market_cap)
 
 
 if __name__ == "__main__":
