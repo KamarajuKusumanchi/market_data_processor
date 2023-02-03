f7a4ed765bbdc370d63429197735d5e9320db5b0
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-02-03 12:33:38 -0500

    lgtm

--- src/nasdaq/get_mktcap_change.py
+++ src/nasdaq/get_mktcap_change.py
@@ -1,5 +1,19 @@
 #! /usr/bin/env python3
 
+# Sample usage
+# $ $market_data_processor/src/nasdaq/get_mktcap_change.py > ~/x/nasdaq.csv
+# $ csvcut -c symbol,marketCap,mktcap_change,pctchange ~/x/nasdaq.csv | column -t -s, -R 2,3,4 | head
+# symbol          marketCap  mktcap_change  pctchange
+# AAPL              2703.38          88.56       3.39
+# AMZN              1096.71         -52.36      -4.56
+# TSLA                622.0          26.29       4.41
+# GOOG              1386.45         -21.74      -1.54
+# GEN                225.42         -20.22      -8.23
+# MSFT              1950.98         -18.65      -0.95
+# META               436.68          10.94       2.57
+# GOOGL             1383.74         -10.74      -0.77
+# NVO                307.59          10.04       3.37
+
 import sys
 import numpy as np
 import pandas as pd
@@ -29,7 +43,8 @@ def run_code():
     df = get_mktcap_change(df)
     pd.set_option("display.max_columns", None, "display.max_rows", None)
     df.index = np.arange(len(df)) + 1
-    df.to_csv(sys.stdout, index=False, lineterminator='\n')
+    # df.to_csv(sys.stdout, index=False, lineterminator='\n')
+    df.round({'marketCap':2, 'mktcap_change':2, 'pctchange':2}).to_csv(sys.stdout, index=False, lineterminator='\n')
 
 
 if __name__ == "__main__":
