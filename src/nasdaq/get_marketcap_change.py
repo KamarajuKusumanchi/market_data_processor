ced1e36c0e2eb0e7d947e1eeb59864a4de4cd312
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-02-08 09:20:54 -0500

    lgtm

--- src/nasdaq/get_marketcap_change.py
+++ src/nasdaq/get_marketcap_change.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python3
 
 # Sample usage
-# $ $market_data_processor/src/nasdaq/get_marketcap_change.py > ~/x/nasdaq.csv
-# $ csvcut -c symbol,marketCap,mktcap_change,pctchange ~/x/nasdaq.csv | column -t -s, -R 2,3,4 | head
-# symbol          marketCap  mktcap_change  pctchange
-# AAPL              2703.38          88.56       3.39
-# AMZN              1096.71         -52.36      -4.56
-# TSLA                622.0          26.29       4.41
-# GOOG              1386.45         -21.74      -1.54
-# GEN                225.42         -20.22      -8.23
-# MSFT              1950.98         -18.65      -0.95
-# META               436.68          10.94       2.57
-# GOOGL             1383.74         -10.74      -0.77
-# NVO                307.59          10.04       3.37
+# $ $market_data_processor/src/nasdaq/get_marketcap_change.py > ~/x/market_cap_change.csv
+# $ csvcut -c symbol,marketCap,marketcap_change,pctchange ~/x/market_cap_change.csv | column -t -s, -R 2,3,4 | head
+# symbol          marketCap  marketcap_change  pctchange
+# MSFT              1991.66             80.31        4.2
+# GOOGL             1378.55              60.7       4.61
+# GOOG              1383.67             58.53       4.42
+# AAPL              2681.22             50.61       1.92
+# NVDA               545.46             26.67       5.14
+# META                496.8             14.41       2.99
+# TSM                490.34             14.11       2.96
+# XOM                 469.1             13.02       2.86
+# BRK/A              698.01             11.11       1.62
 
 import sys
 import numpy as np
@@ -22,19 +22,19 @@ import project_root
 from src.nasdaq.get_nasdaq_data import get_nasdaq_df
 
 
-def get_mktcap_change(df: pd.DataFrame):
+def get_marketcap_change(df: pd.DataFrame):
     # remove rows where either percent change or market cap are missing.
     mask = df["marketCap"].isna() | df["pctchange"].isna()
     df = df[~mask].copy()
     change = df["pctchange"] / 100
-    mktcap_change = (df["marketCap"] * change) / (1 + change)
-    col_name = "mktcap_change"
+    marketcap_change = (df["marketCap"] * change) / (1 + change)
+    col_name = "marketcap_change"
     # insert this after the marketCap column
-    df.insert(df.columns.get_loc("marketCap") + 1, col_name, mktcap_change)
+    df.insert(df.columns.get_loc("marketCap") + 1, col_name, marketcap_change)
     # Sort by the absolute value of market cap change
     df = df.iloc[(-df[col_name].abs()).argsort()].reset_index(drop=True)
     # Put important columns at the beginning
-    important_cols = ['symbol', 'marketCap', 'mktcap_change', 'pctchange']
+    important_cols = ['symbol', 'marketCap', col_name, 'pctchange']
     unimportant_cols = [col for col in df.columns if col not in important_cols]
     df = df[important_cols + unimportant_cols]
     return df
@@ -45,11 +45,11 @@ def run_code():
     in_billions = True
     if in_billions:
         df["marketCap"] /= 1e9
-    df = get_mktcap_change(df)
+    df = get_marketcap_change(df)
     pd.set_option("display.max_columns", None, "display.max_rows", None)
     df.index = np.arange(len(df)) + 1
     # df.to_csv(sys.stdout, index=False, lineterminator='\n')
-    df.round({"marketCap": 2, "mktcap_change": 2, "pctchange": 2}).to_csv(
+    df.round({"marketCap": 2, "marketcap_change": 2, "pctchange": 2}).to_csv(
         sys.stdout, index=False, lineterminator="\n"
     )
 
