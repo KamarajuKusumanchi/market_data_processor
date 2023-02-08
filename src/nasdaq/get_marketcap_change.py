560976b71b944a5a3a5ce5272127341330bfe63e
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-02-08 11:50:04 -0500

    lgtm

--- src/nasdaq/get_marketcap_change.py
+++ src/nasdaq/get_marketcap_change.py
@@ -4,15 +4,15 @@
 # $ $market_data_processor/src/nasdaq/get_marketcap_change.py > ~/x/market_cap_change.csv
 # $ csvcut -c symbol,marketCap,marketcap_change,pctchange ~/x/market_cap_change.csv | column -t -s, -R 2,3,4 | head
 # symbol          marketCap  marketcap_change  pctchange
-# MSFT              1991.66             80.31        4.2
-# GOOGL             1378.55              60.7       4.61
-# GOOG              1383.67             58.53       4.42
-# AAPL              2681.22             50.61       1.92
-# NVDA               545.46             26.67       5.14
-# META                496.8             14.41       2.99
-# TSM                490.34             14.11       2.96
-# XOM                 469.1             13.02       2.86
-# BRK/A              698.01             11.11       1.62
+# GOOGL             1260.85           -117.70      -8.54
+# GOOG              1267.69           -115.98      -8.38
+# AAPL              2636.32            -44.91      -1.68
+# AMZN              1005.78            -33.38      -3.21
+# META               474.89            -21.91      -4.41
+# ASML               267.14             -6.48      -2.37
+# CVX                330.61             -6.02      -1.79
+# FTNT                47.64              5.24      12.37
+# TSLA               627.86              5.14       0.82
 
 import sys
 import numpy as np
@@ -34,9 +34,11 @@ def get_marketcap_change(df: pd.DataFrame):
     # Sort by the absolute value of market cap change
     df = df.iloc[(-df[col_name].abs()).argsort()].reset_index(drop=True)
     # Put important columns at the beginning
-    important_cols = ['symbol', 'marketCap', col_name, 'pctchange']
-    unimportant_cols = [col for col in df.columns if col not in important_cols]
-    df = df[important_cols + unimportant_cols]
+    high_priority_cols = ['symbol', 'marketCap', col_name, 'pctchange']
+    least_priority_cols = ["name"]
+    cols_accounted = high_priority_cols + least_priority_cols
+    cols_unaccounted = [col for col in df.columns if col not in cols_accounted]
+    df = df[high_priority_cols + cols_unaccounted + least_priority_cols]
     return df
 
 
@@ -49,9 +51,15 @@ def run_code():
     pd.set_option("display.max_columns", None, "display.max_rows", None)
     df.index = np.arange(len(df)) + 1
     # df.to_csv(sys.stdout, index=False, lineterminator='\n')
-    df.round({"marketCap": 2, "marketcap_change": 2, "pctchange": 2}).to_csv(
-        sys.stdout, index=False, lineterminator="\n"
-    )
+    # df.round({"marketCap": 2, "marketcap_change": 2, "pctchange": 2}).to_csv(
+    #     sys.stdout, index=False, lineterminator="\n"
+    # )
+    # Using https://stackoverflow.com/a/62546734 to format columns
+    formats = {'marketCap': '{:.2f}', 'marketcap_change': '{:.2f}',
+               'pctchange': '{:.2f}'}
+    for col, f in formats.items():
+        df[col] = df[col].apply(lambda x: f.format(x))
+    df.to_csv(sys.stdout, index=False, lineterminator='\n')
 
 
 if __name__ == "__main__":
