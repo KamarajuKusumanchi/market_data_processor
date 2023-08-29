e7603ac381043b7fd3e9f0495d67da6e633f9650
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2023-08-29 18:37:40 -0400

    lgtm

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def diff_yyyymmdd(dt1: str, dt2: str) -> int:
@@ -9,3 +8,22 @@ def diff_yyyymmdd(dt1: str, dt2: str) -> int:
     date2 = datetime.strptime(dt2, "%Y%m%d")
     diff_days = (date2 - date1).days
     return diff_days
+
+
+def previous_month_end(dt_yyyymmdd: str) -> str:
+    # Given a date in YYYYMMDD form, get the date of previous month end in YYYYMMDD form.
+    # For example
+    #   input,    output
+    #   20230801, 20230731
+    #   20230815, 20230731
+    #   20230831, 20230731
+    #   20230901, 20230831
+    #   20240102, 20231231
+    # If you want to set dt_yyyymmdd to the current date, use
+    #   from datetime import date
+    #   today = date.today().strftime('%Y%m%d')
+    dt = datetime.strptime(dt_yyyymmdd, "%Y%m%d").date()
+    first = dt.replace(day=1)
+    last_month_end = first - timedelta(days=1)
+    last_month_end = last_month_end.strftime("%Y%m%d")
+    return last_month_end
--- tests/src/utils/test_date_utils.py
+++ tests/src/utils/test_date_utils.py
@@ -1,4 +1,6 @@
-from src.utils.date_utils import diff_yyyymmdd
+import pytest
+
+from src.utils.date_utils import diff_yyyymmdd, previous_month_end
 
 
 def test_diff_yyyymmdd():
@@ -7,3 +9,20 @@ def test_diff_yyyymmdd():
     days_got = diff_yyyymmdd(dt1, dt2)
     days_expected = 60
     assert days_got == days_expected
+
+
+@pytest.mark.parametrize(
+    "input_date, output_date_expected",
+    [
+        ("20230801", "20230731"),
+        ("20230815", "20230731"),
+        ("20230831", "20230731"),
+        ("20230901", "20230831"),
+        ("20240102", "20231231"),
+    ],
+)
+def test_previous_month_end(input_date, output_date_expected):
+    output_date_got = previous_month_end(input_date)
+    assert output_date_got == output_date_expected, "Expecting {} but got {}".format(
+        output_date_expected, output_date_got
+    )
