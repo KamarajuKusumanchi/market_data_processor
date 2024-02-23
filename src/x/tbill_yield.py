a4a313710ab4af29715671d2c2b7db981eda924a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-02-23 00:48:57 -0500

    lgtm

--- src/x/tbill_yield.py
+++ src/x/tbill_yield.py
@@ -1,12 +1,46 @@
-def tbill_yield(P, r):
-    # Compute the yield of a tbill that will mature in not more than one half-year.
+def tbill_yield(P, r, y):
+    # Compute the yield of a tbill
     # P = price
     # r = days to maturity
-    y = 366
+    # y = days in year
+    b = r / y
+    if b <= 0.5:
+        i = tbill_yield_short_maturity(P, r, y)
+    else:
+        i = tbill_yield_long_maturity(P, r, y)
+    return i
+
+
+def tbill_yield_short_maturity(P, r, y):
+    # Calculate "Coupon Equivalent Yield" for bills of not more than one
+    # half-year to maturity
+    # P = price
+    # r = days to maturity
+    # y = days in year
+    # Ref:- Example in pg-2 of https://www.treasurydirect.gov/instit/annceresult/press/preanre/2004/ofcalc6decbill.pdf
     assert (
-        r < 0.5 * 366
-    ), "logic not implemented for securities maturing in more than one half-year."
+        r < 0.5 * y
+    ), f"Invalid inputs: r = {r}, y = {y}. Bills should not have more than one half-year to maturity."
     i = ((100 - P) / P) * (y / r)
+    i = round(i * 100, 3)
+    return i
+
+
+def tbill_yield_long_maturity(P, r, y):
+    # Calculate "Coupon Equivalent Yield" for bills of more than one half-year
+    # to maturity
+    # P = price
+    # r = days to maturity
+    # y = days in year
+    # Ref:- Example in pg-2 of https://www.treasurydirect.gov/instit/annceresult/press/preanre/2004/ofcalc6decbill.pdf
+    assert (
+        r > 0.5 * y
+    ), f"Invalid inputs: r = {r}, y = {y}. Bills should have more than one half-year to maturity."
+    b = r / y
+    a = (r / (2 * y)) - 0.25
+    c = (P - 100) / P
+    discriminant = b**2 - 4 * a * c
+    i = (-b + discriminant**0.5) / (2 * a)
     i = round(i * 100, 3)
     return i
 
--- tests/src/x/test_tbill_yield.py
+++ tests/src/x/test_tbill_yield.py
@@ -1,22 +1,68 @@
 import pytest
-from src.x.tbill_yield import tbill_yield
+from src.x.tbill_yield import (
+    tbill_yield,
+    tbill_yield_short_maturity,
+    tbill_yield_long_maturity,
+)
 
 
 @pytest.mark.parametrize(
-    "P, r, yield_expected", [(99.93778, 28, 0.814), (99.5905, 28, 5.375)]
+    "P, r, y, yield_expected",
+    [
+        (99.93778, 28, 366, 0.814),
+        (99.5905, 28, 366, 5.375),
+        (95.252833, 364, 366, 4.950),
+    ],
 )
-def test_tbill_yield(P, r, yield_expected):
-    yield_got = tbill_yield(P, r)
+def test_tbill_yield(P, r, y, yield_expected):
+    yield_got = tbill_yield(P, r, y)
     assert (
         yield_got == yield_expected
-    ), "For P = {}, r = {}, yield_got = {}, yield_expected = {}".format(
-        P, r, yield_got, yield_expected
+    ), "For P = {}, r = {}, y = {}, yield_got = {}, yield_expected = {}".format(
+        P, r, y, yield_got, yield_expected
     )
 
 
-def test_tbill_yield_long_maturity():
-    P = 99.5905
-    r = 183
+@pytest.mark.parametrize(
+    "P, r, y, yield_expected",
+    [
+        (99.93778, 28, 366, 0.814),
+        (99.5905, 28, 366, 5.375),
+    ],
+)
+def test_tbill_yield_short_maturity(P, r, y, yield_expected):
+    yield_got = tbill_yield(P, r, y)
+    assert (
+        yield_got == yield_expected
+    ), "For P = {}, r = {}, y = {}, yield_got = {}, yield_expected = {}".format(
+        P, r, y, yield_got, yield_expected
+    )
+
+
+def test_tbill_yield_short_maturity_with_long_maturity():
+    (P, r, y) = (95.5905, 183, 366)
     # Using the example listed in https://docs.pytest.org/en/latest/how-to/assert.html to catch the AssertionError.
     with pytest.raises(AssertionError):
-        tbill_yield(P, r)
+        tbill_yield_short_maturity(P, r, y)
+
+
+@pytest.mark.parametrize(
+    "P, r, y, yield_expected",
+    [
+        (95.252833, 364, 366, 4.950),
+    ],
+)
+def test_tbill_yield_long_maturity(P, r, y, yield_expected):
+    yield_got = tbill_yield(P, r, y)
+    assert (
+        yield_got == yield_expected
+    ), "For P = {}, r = {}, y = {}, yield_got = {}, yield_expected = {}".format(
+        P, r, y, yield_got, yield_expected
+    )
+
+
+def test_tbill_yield_long_maturity_with_short_maturity():
+    (P, r, y) = (99.5905, 28, 366)
+    # Using the example listed in https://docs.pytest.org/en/latest/how-to/assert.html to catch the AssertionError.
+    with pytest.raises(AssertionError):
+        tbill_yield_long_maturity(P, r, y)
