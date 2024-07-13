d7869c47fd58e1dbb80828833f66ee242beae7e6
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-07-13 13:39:44 -0400

    lgtm

--- src/inprogress/sp500_monthly_returns.py
+++ src/inprogress/sp500_monthly_returns.py
@@ -1,16 +1,26 @@
+import os
 import yfinance as yf
 from datetime import date
+from platformdirs import user_data_dir
+from pathlib import Path
+
+appname = "market_data_processor"
+version = "0.0.1"
+user_data_dir = Path(
+    user_data_dir(appname=appname, version=version, ensure_exists=True)
+)
 
 tickers = ["SPY"]
 start = date(2023, 1, 1)
 end = date(2024, 7, 2)
-daily = yf.download(tickers, start, end)
-# print(daily)
-print("storing daily SPY returns in daily.csv")
-daily.to_csv("daily.csv")
+daily_df = yf.download(tickers, start, end)
+daily_file = os.path.join(user_data_dir, "daily.csv")
+
+print(f"storing daily SPY returns in {daily_file}")
+daily_df.to_csv(daily_file)
 
 
-monthly = daily.resample("ME").agg(
+monthly_df = daily_df.resample("ME").agg(
     {
         "Open": "first",
         "High": "max",
@@ -20,6 +30,6 @@ monthly = daily.resample("ME").agg(
         "Volume": "sum",
     }
 )
-# print(monthly)
-print("storing monthly SPY returns in monthly.csv")
-monthly.to_csv("monthly.csv")
+monthly_file = os.path.join(user_data_dir, "monthly.csv")
+print(f"storing monthly SPY returns in {monthly_file}")
+monthly_df.to_csv(monthly_file)
