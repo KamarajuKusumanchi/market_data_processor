320d39be9a572ab35920b2d86a52984ac33c99a7
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-07-13 19:10:56 -0400

    lgtm

--- src/inprogress/sp500_daily_ohlc.py
+++ src/inprogress/sp500_daily_ohlc.py
@@ -1,6 +1,22 @@
 import yfinance as yf
 from datetime import datetime, date
-df = yf.download(["SPY"], date(2024, 1, 1), date(2024, 3, 31))
+
+# data is not reproducible for these date ranges
+# df = yf.download(["SPY"], date(2023, 12, 1), date(2024, 5, 31))
+# df = yf.download(["SPY"], date(2023, 12, 1), date(2024, 4, 30))
+# df = yf.download(["SPY"], date(2023, 12, 1), date(2024, 2, 29))
+# df = yf.download(["SPY"], date(2023, 12, 1), date(2024, 1, 31))
+# df = yf.download(["SPY"], date(2023, 12, 1), date(2024, 1, 15))
+# df = yf.download(["SPY"], date(2024, 1, 1), date(2024, 1, 15))
+# df = yf.download(["SPY"], date(2024, 1, 8), date(2024, 1, 15))
+df = yf.download(["SPY"], date(2024, 1, 8), date(2024, 1, 11))
+
+# data is reproducible for these date ranges
+# df = yf.download(["SPY"], date(2024, 1, 1), date(2024, 5, 31))
+# df = yf.download(["SPY"], date(2023, 12, 15), date(2024, 1, 15))
+# df = yf.download(["SPY"], date(2024, 1, 1), date(2024, 1, 8))
+
 time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 file_name = f"daily_{time_stamp}.csv"
+print(f"writing data into {file_name}")
 df.to_csv(file_name)
