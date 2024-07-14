cfe4c6aac83ba0f74d067f34ee627653f9ac4ef0
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-07-14 15:15:05 -0400

    lgtm

--- src/inprogress/sp500_daily_ohlc.py
+++ src/inprogress/sp500_daily_ohlc.py
@@ -5,11 +5,14 @@
 import yfinance as yf
 from datetime import datetime, date
 
-yf.enable_debug_mode()
+# yf.enable_debug_mode()
 
-df = yf.download(["SPY"], date(2023, 1, 1), date(2024, 7, 2))
+# df = yf.download(["SPY"], date(2023, 1, 1), date(2024, 7, 2))
+# auto_adjust = True
+auto_adjust = False
+df = yf.download(["SPY"], start=date(2023, 1, 1), end=date(2024, 7, 2), auto_adjust=auto_adjust, progress=False)
 
 time_stamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-file_name = f"daily_{time_stamp}.csv"
+file_name = f"daily_auto_adjust_{auto_adjust}_{time_stamp}.csv"
 print(f"writing data into {file_name}")
 df.to_csv(file_name)
