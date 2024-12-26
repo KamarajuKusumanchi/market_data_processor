f0dbd4c31cfa382b27ec749f1a8bfddfef789512
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-12-25 22:21:41 -0500

    Add shebang

--- src/utils/special_dividend.py
+++ src/utils/special_dividend.py
@@ -1,10 +1,13 @@
+#! /usr/bin/env python
+
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
+import sys
 
 if __name__ == "__main__":
     news = yf.Search("declares special dividend", news_count=10).news
     df = pd.DataFrame(news)
     df["providerPublishTime"] = df["providerPublishTime"].apply(datetime.fromtimestamp)
     df = df[["providerPublishTime", "relatedTickers", "title", "link", "publisher"]]
-    df.to_csv(index=False)
+    df.to_csv(sys.stdout, index=False)
