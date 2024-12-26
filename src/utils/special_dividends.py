0306bbcd9f6801af475059e83a4c602970925fef
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2024-12-25 22:25:35 -0500

    Add sample usage

--- src/utils/special_dividends.py
+++ src/utils/special_dividends.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python
 
+# Sample usage:
+#  % pwd
+# /home/rajulocal/work/github/market_data_processor/src/utils
+#  % mkdir -p ../../../market_data/special_dividends
+#  % ./special_dividends.py > ../../../market_data/special_dividends/special_dividends.csv
+
 import yfinance as yf
 import pandas as pd
 from datetime import datetime
