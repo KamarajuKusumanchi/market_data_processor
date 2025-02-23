a0923a525be4ea07f11f1c6f46df3de03a7d7ac1
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2025-02-22 21:48:02 -0500

    lgtm

--- tests/test_yfinance.py
+++ tests/test_yfinance.py
@@ -5,3 +5,9 @@ def test_ticker_info():
     spy = yf.Ticker("SPY")
     spy_info = spy.info
     assert spy_info is not None
+
+
+def test_news_search():
+    news_count = 10
+    news = yf.Search("declares special dividend", news_count=news_count).news
+    assert len(news) == news_count
