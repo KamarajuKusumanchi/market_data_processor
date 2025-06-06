5448dbd366b3924c9d7f297f3b89bd796f575ff7
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2025-06-06 19:39:36 -0400

    lgtm

--- tests/test_yfinance.py
+++ tests/test_yfinance.py
@@ -5,7 +5,7 @@ from packaging.version import Version
 def test_check_version():
     # using the recipe in https://stackoverflow.com/questions/11887762/how-do-i-compare-version-numbers-in-python to compare package versions.
     installed_version = Version(yf.__version__)
-    required_version = Version("0.2.54")
+    required_version = Version("0.2.61")
     assert (
         installed_version >= required_version
     ), f"Installed version of yfinance is {installed_version}. But at least version {required_version} is required."
