2e6ce611269ede4940d7e677914531d0cc96f253
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2026-09-18 17:32:48 -0400

    lgtm

--- src/scripts/money_stuff_urls.py
+++ src/scripts/money_stuff_urls.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python3
 # script to fetch Matt Levine's Money Stuff article URLs from Bloomberg.
 # Sample usage:
+# raju@DESKTOP-CRLPVE5 MINGW64 ~
+# $ conda activate market_data_processor
+# $ python $market_data_processor/src/scripts/money_stuff_urls.py --page 1
+#
 # rajulocal@hogwarts ~/work/github/market_data/money_stuff_urls
 #  % $market_data_processor/src/scripts/money_stuff_urls.py --page 1 > 1.txt
 import sys
